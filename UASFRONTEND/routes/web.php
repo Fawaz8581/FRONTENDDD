0a6d0f58e58f0513e421a038270dacc9b3ef1794
Fawaz8581 <ffawaz8581@gmail.com>
2024-11-18 22:17:28 +0700

    Added HTML, CSS, JS, IMG (source from midterm Frontend)

--- UASFRONTEND/routes/web.php
+++ UASFRONTEND/routes/web.php
@@ -3,5 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/cancer', function () {
+    return view('cancer');
+});
+
+Route::get('/cardiology', function () {
+    return view('cardiology');
+});
+
+
+Route::get('/dentistry', function () {
+    return view('dentistry');
+});
+
+
+Route::get('/dermatology', function () {
+    return view('dermatology');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/neurology', function () {
+    return view('neurology');
+});
+
+Route::get('/ophthalmology', function () {
+    return view('ophthalmology');
 });
